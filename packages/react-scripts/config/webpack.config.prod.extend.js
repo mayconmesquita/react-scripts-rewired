fb0838790b6ad10afecb4add8be55c184d9bef51
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-05-09 08:47:52 +0200

    Make extension files optional in the project codebase

--- packages/react-scripts/config/webpack.config.prod.extend.js
+++ packages/react-scripts/config/webpack.config.prod.extend.js
@@ -1,7 +1,14 @@
+/* eslint-disable */
 
 const paths = require('./paths');
 const configFactory = require('./webpack.config');
 const baseConfig = configFactory('production');
-const extendConfigFn = require(paths.appPath + '/webpack.config.extend.js')
+
+// make the extension file optional
+const fs = require('fs-extra');
+const extensionPath = paths.appPath + '/webpack.config.extend.js'
+const extendConfigFn = fs.existsSync(extensionPath)
+    ? require(extensionPath)
+    : $ => $;
 
 module.exports = extendConfigFn(baseConfig, process.env.NODE_ENV, { paths })
--- packages/react-scripts/config/webpackDevServer.config.extend.js
+++ packages/react-scripts/config/webpackDevServer.config.extend.js
@@ -1,7 +1,14 @@
+/* eslint-disable */
 
 const paths = require('./paths');
 const createBaseConfig = require('./webpackDevServer.config');
-const extendConfigFn = require(paths.appPath + '/webpackDevServer.config.extend.js')
+
+// make the extension file optional
+const fs = require('fs-extra');
+const extensionPath = paths.appPath + '/webpackDevServer.config.extend.js'
+const extendConfigFn = fs.existsSync(extensionPath)
+    ? require(extensionPath)
+    : $ => $;
 
 module.exports = function(proxy, allowedHost) {
     const baseConfig = createBaseConfig(proxy, allowedHost)
